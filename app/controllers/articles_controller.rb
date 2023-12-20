7eda50cfb4c9f15bc8c85719a9abcc87642b48ea
Паращевин Григорий Андреевич <gparaschevin@stsoft.ru>
2023-12-20 14:47:20 +0300

    add i18n static_page#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,19 @@
 class ArticlesController < ApplicationController
   def index
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
