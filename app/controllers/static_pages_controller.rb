03497ada516291fb773e01bfb33558fdcb2c1d4f
Паращевин Григорий Андреевич <gparaschevin@stsoft.ru>
2023-12-20 15:25:18 +0300

    add dynamical title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,24 @@
 class StaticPagesController < ApplicationController
   def index
+    @current_page = t(:label_home)
   end
 
   def about
-
+    @current_page = t(:label_about)
   end
 
   def articles
-
   end
 
   def projects
-
+    @current_page = t(:label_projects)
   end
 
   def skills
+    @current_page = t(:label_skills)
+  end
+
+  def tools
+    @current_page = t(:label_tools)
   end
 end
