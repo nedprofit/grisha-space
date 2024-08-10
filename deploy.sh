480268ffed92f7d557fcd55d7a885ba9be71c3cb
Grigoriy Paraschevin <gparashchevin@optimalcity.ru>
2024-08-10 14:21:35 +0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+#!/bin/bash
+git config --global --add safe.directory /sites/grisha-space
+
+# Пересобираем и перезапускаем контейнеры
+docker compose down
+
 # Стягиваем последние изменения из репозитория
 git pull origin main
 
-# Пересобираем и перезапускаем контейнеры
-docker-compose down
-docker-compose up --build -d
+export RAILS_MASTER_KEY=fec7452e23abaf4d469f7ff0a9a281dd
+docker compose up --build -d
+
+# Логируем успех
+echo "Deployed successfully at $(date)" >> /var/log/webhook_logs/webhook_deploy.log
 
 # Опционально: удалить неиспользуемые Docker образы и контейнеры
 docker system prune -f
-
-# Логируем успех
-echo "Deployed successfully at $(date)" >> /var/log/webhook_deploy.log
