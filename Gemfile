0e6fdd656ffa2e54148cf762ab7f218fca3c6b5c
Grigoriy Paraschevin <gparashchevin@optimalcity.ru>
2025-03-12 23:59:48 +0300

    Migrate to Propshaft

--- Gemfile
+++ Gemfile
@@ -6,8 +6,7 @@ ruby "3.4.1"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails"
 
-# The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
-gem "sprockets-rails"
+gem "propshaft"
 
 # Use sqlite3 as the database for Active Record
 gem 'pg'
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module GrishaSpace
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+
+    config.asset_finder = InlineSvg::PropshaftAssetFinder
+
     config.i18n.default_locale = :ru
 
     # Configuration for the application, engines, and railties goes here.
